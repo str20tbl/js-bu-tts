f37374130f9b7d4130aed1a5fe99a2c4555dc0c3
Stephen Russell <str20tbl@bangor.ac.uk>
2023-06-30 05:56:43 +0100

    synthesize function tts.js

---
+++ tts.js
@@ -0,0 +1,18 @@
+function q(selector) {
+    return document.querySelector(selector)
+}
+
+export function synthesize(text, speaker_id = "") {
+    let base_url = "https://api.techiaith.org/coqui-tts/api/";
+    let uri_text = text;
+    let uri_speaker_id = speaker_id;
+    let api_key = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxx";
+    let url = `${base_url}v1?testun=${uri_text}&siaradwr=${uri_speaker_id}&api_key=${api_key}`;
+    fetch(encodeURI(url)).then(function (res) {
+        if (!res.ok) throw Error(res.statusText);
+        return res.blob();
+    }).then(function (blob) {
+        q('#audio').src = URL.createObjectURL(blob);
+        q('#audio').hidden = false;
+    });
+}
